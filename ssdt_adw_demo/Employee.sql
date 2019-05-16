bd8877def2c8637c21c0a58401a6b0b09a59cf83
Kumar Krishnamurthy <kumar.sendhi@gmail.com>
2019-05-16 13:55:53 +0000

    v12 to v0

--- ssdt_adw_demo/Employee.sql
+++ ssdt_adw_demo/Employee.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[Employee]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [Employee_Name] NVARCHAR(MAX) NOT NULL
+    [Employee_Name] NVARCHAR(MAX) NOT NULL, 
+    [Age] NVARCHAR(MAX) NOT NULL
 )
